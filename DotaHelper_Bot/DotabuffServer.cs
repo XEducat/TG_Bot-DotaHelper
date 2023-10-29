b95f5fcca18375d2ac86c7ac3b1646486b40fadd
XEducat <113421299+XEducat@users.noreply.github.com>
2023-10-29 14:07:11 +0200

    Change DotabuffSerever.cs AND small refactored TelegramBot.cs
    -- Packages --
    HtmlAgiltyPack updated 1.11.46 -> 1.11.54
    Azure.Identity removed from proj.
    Azure.Core ed from proj.
    ----------------
    
    -- DotabuffServer,.cs --
    Full Refactored
    We can conveniently set the displayed number of heroes
    --------------------------
    
    -- TelegramBot --
    Added setup function, for separate the setup logic from Contructor.
    ---------------------

--- DotaHelper_Bot/DotabuffServer.cs
+++ DotaHelper_Bot/DotabuffServer.cs
@@ -1,72 +1,46 @@
-﻿using HtmlAgilityPack;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Net;
+using System.Net.Http;
+using HtmlAgilityPack;
+
 
 namespace DotaHelper_Bot
 {
+    // TODO LIST:
+    // Отделить логику от визуализации в методах:
+    // - UpdateInfo()
+    // - ExtractHeroesDataFromDotabuff()
+
     public class DotabuffServer
     {
-        private string SiteUrl {get; } = "https://www.dotabuff.com";
+        private readonly HttpClient httpClient;
+        private const string SiteUrl = "https://www.dotabuff.com";
 
-        private HeroesDB database = new HeroesDB();
+        private HeroesDB database;
 
         public DotabuffServer()
         {
+            database = new HeroesDB();
+            httpClient = new HttpClient();
+            httpClient.DefaultRequestHeaders.Add("User-Agent", "YourUserAgent/1.0");
+
             UpdateInfo();
-        }
-
-        public List<Hero> GetHeroCounterpeaks(string text)
-        {
-            // Searching for a character in the database
-            var hero = GetHero(text);
-            if (hero.Name == null) return null;
-
-            HtmlWeb web = new HtmlWeb();
-            string url = $"{SiteUrl}/heroes/{hero.Name.ToLower().Replace(" ", "-").Replace("`", "")}/counters";
-            HtmlDocument doc = web.Load(url); // (Example: https://www.dotabuff.com/heroes/night-stalker/counters)
-
-            List<Hero> result = new List<Hero>();
-
-            // Searching for counterpeaks in HTML and entering them into a variable result
-            foreach (var item in doc.DocumentNode.SelectNodes("/html/body/div[2]/div[2]/div[3]/div[4]/div[1]/div[1]/section/article/table/tbody/tr/td[2]"))
-            { 
-                result.Add(new Hero { Name = item.InnerText.Trim().Replace("&#39;", "`") });
-            }
-
-            return result;
         }
 
         public void UpdateInfo()
         {
-            List<Hero> heroes = new List<Hero>();
-            
-            HtmlWeb web = new HtmlWeb();
-            HtmlDocument doc = web.Load($"https://www.dotabuff.com/heroes/");
+            List<Hero> heroes = ExtractHeroesDataFromDotabuff();
 
-            // Finding the character and its image to add to the List<Hero>
-            foreach (var item in doc.DocumentNode.SelectNodes("//div[@class='hero']"))
+            if (heroes.Count > 0)
             {
-                string ImageUrl = SiteUrl + item.Attributes["style"].Value.Replace("background: url(", "").Replace(")", "");
-                string heroName = item.InnerText.Trim().Replace("&#39;", "`").Replace("-", " ");
-
-                string fileLocation = $"../../Resources/Heroes_Icons/{heroName.Replace(" ", "_")}Icon.jpg";
-
-                //If the image is on the device, then add it to the list
-                if (DownloadWebsiteImage(ImageUrl, fileLocation))   
-                {
-                    heroes.Add(new Hero()
-                    {
-                        Path = fileLocation,
-                        Name = heroName
-                    });
-                }
+                database.AddHeroesData(heroes);
+                Console.WriteLine("Information UPDATED");
             }
-
-            // Transfer the received data to the database
-            database.AddHeroesData(heroes);
-            Console.WriteLine("Information UPDATED");
+            else
+            {
+                Console.WriteLine("No hero data found.");
+            }
         }
 
         public Hero GetHero(string text)
@@ -75,67 +49,117 @@ namespace DotaHelper_Bot
 
             return database.GetHeroData(text);
         }
-        
-        private bool DownloadWebsiteImage(string Url, string FilePath)
+
+        public List<Hero> GetHeroCounterpeaks(string text)
         {
-            // Checking if the file exists on the device
+            List<Hero> result = null;
+            var hero = GetHero(text);
+
+            if (hero.Name != null)
+            {
+                string url = BuildHeroCounterpeaksUrl(hero.Name);
+                result = ExtractCounterpeaksFromURL(5, url);
+            }
+
+            return result;
+        }
+
+        private string BuildHeroCounterpeaksUrl(string heroName)
+        {
+            string normalizedHeroName = heroName.ToLower().Replace(" ", "-").Replace("`", "");
+            return $"{SiteUrl}/heroes/{normalizedHeroName}/counters";
+        }
+
+        private List<Hero> ExtractCounterpeaksFromURL(int Count, string url)
+        {
+            List<Hero> result = new List<Hero>();
+            HtmlDocument doc = LoadHtmlDocument(url);
+
+            foreach (var item in doc.DocumentNode.SelectNodes("//table/tbody/tr/td[2][not(number(.) = number(.))]"))
+            {
+                result.Add(new Hero { Name = item.InnerText.Trim().Replace("&#39;", "`") });
+
+                if (result.Count >= Count)
+                {
+                    break;
+                }
+            }
+
+            return result;
+        }
+
+        // TODO: Отделить логику от визуализации
+        private List<Hero> ExtractHeroesDataFromDotabuff()
+        {
+            List<Hero> heroes = new List<Hero>();
+            HtmlDocument doc = LoadHtmlDocument($"{SiteUrl}/heroes/");
+
+            foreach (var heroNode in doc.DocumentNode.SelectNodes("//div[@class='hero']"))
+            {
+                Hero hero = ExtractHero(heroNode);
+                heroes.Add(hero);
+            }
+
+            return heroes;
+        }
+
+        private HtmlDocument LoadHtmlDocument(string url)
+        {
+            HtmlWeb web = new HtmlWeb();
+            return web.Load(url);
+        }
+
+        private Hero ExtractHero(HtmlNode node)
+        {
+            string heroName = ExtractHeroName(node);
+            string imageUrl = ExtractHeroImageUrl(node);
+            string fileLocation = GetHeroIconFilePath(heroName);
+
+            if (DownloadImage(imageUrl, fileLocation))
+                Console.WriteLine($"Картинку {heroName} успішно завантажено та збережено.");
+
+            return new Hero() { Path = fileLocation, Name = heroName };
+        }
+
+        // Витягує URL на зображення героя з HTML-елемента.
+        private string ExtractHeroImageUrl(HtmlNode item)
+        {
+            string styleAttribute = item.Attributes["style"].Value;
+            return SiteUrl + styleAttribute.Replace("background: url(", "").Replace(")", "");
+        }
+
+        // Витягує ім'я героя з HTML-елемента.
+        private string ExtractHeroName(HtmlNode item)
+        {
+            return item.InnerText.Trim().Replace("&#39;", "`").Replace("-", " ");
+        }
+
+        // Генерує шлях до файлу іконки героя на основі імені героя.
+        private string GetHeroIconFilePath(string heroName)
+        {
+            return $"../../Resources/Heroes_Icons/{heroName.Replace(" ", "_")}Icon.jpg";
+        }
+
+        // Завантажує зображення з вказаного URL та зберігає його на пристрої.
+        private bool DownloadImage(string imageUrl, string fileLocation)
+        {
+            bool imageDownloaded = false;
+
             try
             {
-                var n = System.IO.File.Open(FilePath, FileMode.Open);
-            }
-            catch
+                if (!File.Exists(fileLocation))
+                {
+                    byte[] imageBytes = httpClient.GetByteArrayAsync(imageUrl).Result;
+                    File.WriteAllBytes(fileLocation, imageBytes);
+                    imageDownloaded = true;
+                }
+            } 
+            catch (Exception ex)
             {
-                // Making a request to the specified URL
-                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(Url);
-                request.Method = "Get";
-                request.Accept = "*/*";
-                request.Host = "www.dotabuff.com";
-                request.UserAgent = "PostmanRuntime/7.29.2";
-                HttpWebResponse response = null;
-                try
-                {
-                    response = (HttpWebResponse)request.GetResponse();
-                }
-                catch (Exception)
-                {
-                    Console.WriteLine("Image not found on the site");
-                    return false;
-                }
-
-                // Check sure the deleted file from the site is found.
-                // The ContentType check is performed since a request for 
-                // a non-existent image file might be redirected to a 404-page,
-                // which would yield the StatusCode "OK", even though the image
-                // was not found.
-                if ((response.StatusCode == HttpStatusCode.OK ||
-                    response.StatusCode == HttpStatusCode.Moved ||
-                    response.StatusCode == HttpStatusCode.Redirect) &&
-                    response.ContentType.StartsWith("image", StringComparison.OrdinalIgnoreCase))
-                    {
-
-                    // if the remote file was found, download it to the device
-                    using (Stream inputStream = response.GetResponseStream())
-                    using (Stream outputStream = System.IO.File.OpenWrite(FilePath))
-                    {
-                        byte[] buffer = new byte[4096];
-                        int bytesRead;
-                        do
-                        {
-                            bytesRead = inputStream.Read(buffer, 0, buffer.Length);
-                            outputStream.Write(buffer, 0, bytesRead);
-                        } while (bytesRead != 0);
-                    }
-                    return true;
-                }
-                else
-                {
-                    Console.WriteLine("Image could not be loaded");
-                    return false;
-                }
+                Console.WriteLine("Помилка завантаження зображення: " + ex.Message);
             }
 
-            // We have an image on the device
-            return true;
+            return imageDownloaded;
         }
     }
 }
